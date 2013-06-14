a9dee5510655cad43e48ff27bda0fbbcd385e01a
Kurt Nelson <kurtisnelson@gmail.com>
2013-06-14 11:21:14 -0400

    Create the gem

---
+++ lib/grocer/pushpackager.rb
@@ -0,0 +1,7 @@
+require "grocer/pushpackager/version"
+
+module Grocer
+  module Pushpackager
+    # Your code goes here...
+  end
+end
---
+++ lib/grocer/pushpackager/version.rb
@@ -0,0 +1,5 @@
+module Grocer
+  module Pushpackager
+    VERSION = "0.0.1"
+  end
+end
